154c82de2c4908957791af78eb903766cc834a9d
Briana Fernandez <98505287+briifernandez@users.noreply.github.com>
2022-05-01 20:14:15 -0700

    Updates!

--- practiceDemo.java
+++ practiceDemo.java
@@ -1,3 +1,7 @@
 public class practiceDemo {
 
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
 }
---
+++ practiceDemoTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class practiceDemoTest {
+
+    @Test 
+    public void multiply() {
+
+        assertEquals(3, practiceDemo.multiply(3, 2));
+
+    }
+
+    
+}
